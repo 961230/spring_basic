09b552ea72c27c765383cb76f9eb803039403b1b
hansolShin <hansolstar@naver.com>
2021-05-04 17:55:59 +0900

    빈 생명주기 콜백 / @Been init | @Post/@Pre사용

--- core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -16,6 +16,10 @@ public class BeanLifeCycleTest {
     }
     @Configuration
     static class LifeCycleConfig{
+        // destroyMethod의 defalut는 (inferred)로 close나 shotdown의 이름의 Method를 추론 가능
+        // 수정할 수 없는 외부 라이브러리에 적용할 떄 사용
+        //@Bean(initMethod = "init", destroyMethod = "close")
+        //@Bean(initMethod = "init")
         @Bean
         public NetworkClient networkClient(){
             NetworkClient  networkClient = new NetworkClient();
